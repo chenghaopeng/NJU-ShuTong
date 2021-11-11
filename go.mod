d3fca1868cff64c188fd14fe5a3ae13542fc7948
chenghaopeng <i@chper.cn>
2021-11-11 17:35:46 +0800

    新增每日健康打卡监测功能；寝室电量监测修改一些细节

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module app
 
 go 1.17
+
+require github.com/robfig/cron/v3 v3.0.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -13,10 +14,13 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 var EAI_SESS = os.Getenv("EAI_SESS")
 var ROOM = strings.ReplaceAll(os.Getenv("ROOM"), " ", "")
+var MOD_AUTH_CAS = os.Getenv("MOD_AUTH_CAS")
 
 var wg sync.WaitGroup
 
@@ -68,17 +72,38 @@ func getElectricBalance(areaId string, buildId string, roomId string) (float64, 
 	reg := regexp.MustCompile(`dianyue\w*:\w*"([\d\.]+)"`)
 	result := reg.FindStringSubmatch(body)
 	if len(result) != 2 {
-		return 0, errors.New("登录失效或未找到电量余额")
+		return 0, errors.New("EAI_SESS 失效了")
 	}
 	return strconv.ParseFloat(result[1], 64)
+}
+
+func getLatestHealthReport() (checked bool, err error) {
+	defer func() {
+		if err2 := recover(); err2 != nil {
+			err = errors.New(fmt.Sprint(err2))
+		}
+	}()
+	body, err := doGet("http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do", map[string]string{
+		"Cookie": "MOD_AUTH_CAS=" + MOD_AUTH_CAS,
+	})
+	if err != nil {
+		return false, errors.New("MOD_AUTH_CAS 失效了")
+	}
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(body), &result)
+	if err != nil {
+		return false, err
+	}
+	var report map[string]interface{} = result["data"].([]interface{})[0].(map[string]interface{})
+	return report["TBZT"] == "1", nil
 }
 
 func taskElectricBalance() {
 	wg.Add(1)
 	defer wg.Done()
 	ids := strings.SplitN(ROOM, ",", 3)
-	if len(ids) != 3 {
-		sendMessage("未设定寝室或者设定有误，不开启寝室电量检测")
+	if len(EAI_SESS) == 0 || len(ids) != 3 {
+		sendMessage("寝室未配置或配置错误，或未配置 EAI_SESS，不开启寝室电量监测")
 		return
 	}
 	sendMessage("书童开始为你监测寝室电量啦～")
@@ -106,8 +131,39 @@ func taskElectricBalance() {
 	}
 }
 
+func taskHealthReport() {
+	wg.Add(1)
+	defer wg.Done()
+	if len(MOD_AUTH_CAS) == 0 {
+		sendMessage("未配置 MOD_AUTH_CAS，不开启每日健康打卡监测")
+		return
+	}
+	sendMessage("书童开始为你监测每日健康打卡啦～")
+	sentErr := false
+	c := cron.New()
+	c.AddFunc("0 10,22 * * *", func() {
+		checked, err := getLatestHealthReport()
+		log.Println("taskHealthReport", checked, err)
+		if err != nil {
+			if !sentErr {
+				sentErr = true
+				sendMessage("获取每日健康打卡出错了：" + err.Error())
+			}
+		} else {
+			sentErr = false
+			if !checked {
+				sendMessage("记得今天的每日健康打卡喔～")
+			}
+		}
+	})
+	c.Start()
+	defer c.Stop()
+	select {}
+}
+
 func main() {
 	go taskElectricBalance()
+	go taskHealthReport()
 	time.Sleep(time.Second)
 	wg.Wait()
 }
