8cb80dab156801b59267138a370cdd01fb70f1de
chenghaopeng <i@chper.cn>
2021-11-11 16:25:30 +0800

    优化代码结构

--- main.go
+++ main.go
@@ -11,11 +11,14 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 var EAI_SESS = os.Getenv("EAI_SESS")
-var ROOM = os.Getenv("ROOM")
+var ROOM = strings.ReplaceAll(os.Getenv("ROOM"), " ", "")
+
+var wg sync.WaitGroup
 
 func execScript(name string, env []string) {
 	cmd := exec.Cmd{
@@ -24,12 +27,16 @@ func execScript(name string, env []string) {
 		Env:  env,
 	}
 	output, err := cmd.Output()
-	log.Println(string(output), err)
+	log.Println("execScript", name, env, string(output), err)
 }
 
-func getElectricBalance() (float64, error) {
-	ids := strings.SplitN(ROOM, ",", 3)
-	url := fmt.Sprintf("https://wx.nju.edu.cn/njucharge/wap/electric/charge?area_id=%s&build_id=%s&room_id=%s", ids[0], ids[1], ids[2])
+func sendMessage(content string) {
+	execScript("message", []string{"CONTENT=" + content})
+	log.Println("sendMessage", content)
+}
+
+func getElectricBalance(areaId string, buildId string, roomId string) (float64, error) {
+	url := fmt.Sprintf("https://wx.nju.edu.cn/njucharge/wap/electric/charge?area_id=%s&build_id=%s&room_id=%s", areaId, buildId, roomId)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return 0, err
@@ -54,25 +61,30 @@ func getElectricBalance() (float64, error) {
 	return strconv.ParseFloat(result[1], 64)
 }
 
-func main() {
-	send := func(content string) {
-		execScript("electric", []string{"CONTENT=" + content})
+func taskElectricBalance() {
+	wg.Add(1)
+	defer wg.Done()
+	ids := strings.SplitN(ROOM, ",", 3)
+	if len(ids) != 3 {
+		sendMessage("未设定寝室或者设定有误，不开启寝室电量检测")
+		return
 	}
+	sendMessage("书童开始为你监测寝室电量啦～")
 	sentErr, sentEle := false, false
 	for {
-		electric, err := getElectricBalance()
-		log.Println(electric, err)
+		electric, err := getElectricBalance(ids[0], ids[1], ids[2])
+		log.Println("taskElectricBalance", electric, err)
 		if err != nil {
 			if !sentErr {
 				sentErr = true
-				send("获取电量出错了：" + err.Error())
+				sendMessage("获取寝室电量出错了：" + err.Error())
 			}
 		} else {
 			sentErr = false
 			if electric <= 10 {
 				if !sentEle {
 					sentEle = true
-					send("电量不足啦～")
+					sendMessage("寝室电量不足 10 度啦～")
 				}
 			} else {
 				sentEle = false
@@ -80,4 +92,10 @@ func main() {
 		}
 		time.Sleep(time.Minute)
 	}
+}
+
+func main() {
+	go taskElectricBalance()
+	time.Sleep(time.Second)
+	wg.Wait()
 }
